9a8382b8f2d6f92f00a3971396351e3eb4948a2c
lxpwr <149712377+lxpwr@users.noreply.github.com>
2023-12-11 08:34:42 +0300

    Update nginxrest.sh

--- nginxrest.sh
+++ nginxrest.sh
@@ -4,4 +4,4 @@ cp /home/vagrant/nginx.spec /root/rpmbuild/SPECS/nginx.spec
 nginx -t
 nginx -s reload
 echo "Ставим МС из репо"
-yum install /root/rpmbuild/RPMS/x86_64/mc-4.8.27-1.1.x86_64.rpm -y
+yum install -y mc-4.8.27-1.1.x86_64.rpm
